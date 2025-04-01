95a836541c9b81b563cfb05caddcc0b4f78c3a97
bhanderikhushi866 <bhanderikhushi866@gmail.com>
2025-04-01 10:53:36 +0530

    commited

---
+++ ExceptionHandling/demoExceptionHandling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NB_23FOTCA11003.ExceptionHandling
+{
+     class demoExceptionHandling
+    {
+        public static void Main(string[] args)
+        {
+            int no1, no2, result;
+
+            Console.WriteLine("Enter number 1 :");
+            no1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number 1 :");
+            no2 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                result = no1 / no2;
+                Console.WriteLine("Division of two number :" + result);
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            result = no1  + no2;
+            Console.WriteLine("addition of two number :" + result);
+
+        }
+
+    }
+}
